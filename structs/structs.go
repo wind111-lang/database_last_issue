b276fa1d3401f01e49418a1b3b3eadc282a22be8
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-06-22 10:13:53 +0900

    Add: Some structs

---
+++ structs/structs.go
@@ -0,0 +1,32 @@
+package structs
+
+type Translate struct {
+	Text string `json:"text"`
+	To   string `json:"to"`
+}
+type TranslationRes struct {
+	Translation []Translate `json:"translations"`
+}
+
+// type ChatLog struct {
+// 	Name string `gorm:"name"`
+// 	Text string `gorm:"text"`
+// 	Time string `gorm:"time"`
+// }
+
+type User struct {
+	//gorm.Model
+	Username string `form:"username" binding:"required" gorm:"unique;not null"`
+	Password string `form:"password" binding:"required"`
+	Birthday string `form:"birthday" binding:"required"`
+}
+
+type JsonMessage struct {
+	Name    string `json:"name"`
+	Message string `json:"message"`
+}
+
+type JsonReturn struct {
+	Name string `json:"name"`
+	Text string `json:"text"`
+}
