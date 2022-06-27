40ee32be33469c305bfc21963b3b73185d7b8011
Shota Tsutsui <k20076kk@aitech.ac.jp>
2022-06-27 11:21:44 +0900

    Add:GetIP func

---
+++ websock/getip.go
@@ -0,0 +1,22 @@
+package websock
+
+import (
+	"fmt"
+	"net"
+)
+
+var ip string
+
+func GetIP() string {
+	netInterfaceaddr, _ := net.InterfaceAddrs()
+
+	for _, addr := range netInterfaceaddr {
+		netIP, ok := addr.(*net.IPNet)
+
+		if ok && !netIP.IP.IsLoopback() && netIP.IP.To4() != nil {
+			ip = netIP.IP.String()
+			fmt.Println(ip)
+		}
+	}
+	return ip
+}
