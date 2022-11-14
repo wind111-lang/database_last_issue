a7bfaaca446f37d9bf596d48bb71a38eb7b1a93b
wind111-lang <k20076kk@aitech.ac.jp>
2022-11-14 15:32:49 +0900

    dsn setting

--- db/db.go
+++ db/db.go
@@ -23,7 +23,7 @@ func gormConnect() *gorm.DB {
 	name := os.Getenv("dbname")
 	port := os.Getenv("port")
 
-	dsn := "host=" + hostname + " user=" + user + " password=" + password + " dbname=" + name + " port=" + port + " sslmode=disabled TimeZone=Asia/Tokyo"
+	dsn := "host=" + hostname + " user=" + user + " password=" + password + " dbname=" + name + " port=" + port + " sslmode=enabled TimeZone=Asia/Tokyo"
 	fmt.Println(dsn)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
